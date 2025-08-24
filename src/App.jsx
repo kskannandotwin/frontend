c72c5c22ee0f614dd4e716f25beebcf50e085674
Jobulikannan <kannan@jobuli.com>
2025-08-24 12:43:36 +0530

    24.08.2025
    cr completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import "./App.css";
-import MovieCard from "./components/MovieCard"
 
 function App() {
+
+
   return (
     <>
-      <div>
-        <MovieCard movie={{ title: "Tim's film", release_date: "2024" }} />
-        <MovieCard movie={{ title: "Joe's film", release_date: "2020" }} />
-      </div>
+
     </>
   )
 }
