3d71c5e1bf47feba479364c109fb4bdf2e7326ca
kannanATexcelforte <kannan@excelforte.com>
2025-08-24 15:59:37 +0530

    24.08.2025
    pr completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import "./App.css";
 import Home from "./pages/Home";
+import Favorites from "./pages/Favorites";
+import NavBar from "./components/NavBar";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <Home />
+      <NavBar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="favorites" element={<Favorites />} />
+        </Routes>
+      </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
